5dbeb92d282f783e400665733f3f15f71a4327df
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-04-14 17:57:09 +0530

    added some data.

---
+++ src/Data/projects.js
@@ -0,0 +1,68 @@
+const projectsData = [
+  {
+    no: 1,
+    projectName: "",
+    description: "",
+    technologiesUsed: "",
+    liveAt: "",
+    githubRepo: ""
+  },
+  {
+    no: 2,
+    projectName: "",
+    description: "",
+    technologiesUsed: "",
+    liveAt: "",
+    githubRepo: ""
+  },
+  {
+    no: 3,
+    projectName: "",
+    description: "",
+    technologiesUsed: "",
+    liveAt: "",
+    githubRepo: ""
+  },
+  {
+    no: 4,
+    projectName: "",
+    description: "",
+    technologiesUsed: "",
+    liveAt: "",
+    githubRepo: ""
+  },
+  {
+    no: 5,
+    projectName: "",
+    description: "",
+    technologiesUsed: "",
+    liveAt: "",
+    githubRepo: ""
+  },
+  {
+    no: 6,
+    projectName: "",
+    description: "",
+    technologiesUsed: "",
+    liveAt: "",
+    githubRepo: ""
+  },
+  {
+    no: 7,
+    projectName: "",
+    description: "",
+    technologiesUsed: "",
+    liveAt: "",
+    githubRepo: ""
+  },
+  {
+    no: 8,
+    projectName: "",
+    description: "",
+    technologiesUsed: "",
+    liveAt: "",
+    githubRepo: ""
+  }
+];
+
+export default projectsData;
