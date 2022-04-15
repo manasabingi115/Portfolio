33a096b844488dfde2fafa74f2a91629ee0c17c1
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-04-15 16:28:23 +0530

    modified code.

--- src/header.js
+++ src/header.js
@@ -20,33 +20,45 @@ export default function Header() {
       </div>
     );
   }
+  function MobileMenu() {
+    return (
+      <div className="mobile-menu">
+        <ImCancelCircle onClick={() => setMobileMenu(false)} />
+        {menuList.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
+    );
+  }
+
   return (
-    <div className="header">
-      <h1 className="name">Manasa Bingi</h1>
-      <Menu />
-      <div className="view-resume-parent-and-menu-icon">
-        <p className="view-resume">
-          <a
-            href="https://docs.google.com/document/d/1UdAur_a4h_9o5A2XBAabc4m2tfEFE_h_4RGqSbTKIk0/edit?usp=sharing"
-            target="_blank"
-            className="view-resume-text"
-          >
-            View Resume
-          </a>
-        </p>
-        <div className="menu-icon-parent">
-          <GoThreeBars
-            className="menu-icon"
-            onClick={() => setMobileMenu(true)}
-          />
-          <br />
-          {mobileMenu && (
-            <ImCancelCircle onClick={() => setMobileMenu(false)} />
-          )}
-          {mobileMenu &&
-            menuList.map((item, index) => <p key={index}>{item}</p>)}
+    <div>
+      <div className="header">
+        <h1 className="name">Manasa Bingi</h1>
+        <Menu />
+        <div className="view-resume-parent-and-menu-icon">
+          <p className="view-resume">
+            <a
+              href="https://docs.google.com/document/d/1UdAur_a4h_9o5A2XBAabc4m2tfEFE_h_4RGqSbTKIk0/edit?usp=sharing"
+              target="_blank"
+              className="view-resume-text"
+            >
+              View Resume
+            </a>
+          </p>
+          <div className="menu-icon-parent">
+            <GoThreeBars
+              className="menu-icon"
+              onClick={() => setMobileMenu(true)}
+            />
+          </div>
         </div>
       </div>
+      {mobileMenu && <MobileMenu />}
+      {/* <div className="mobile-menu">
+        {mobileMenu && <ImCancelCircle onClick={() => setMobileMenu(false)} />}
+        {mobileMenu && menuList.map((item, index) => <p key={index}>{item}</p>)}
+      </div> */}
     </div>
   );
 }
