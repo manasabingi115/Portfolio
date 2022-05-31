335ccfe99740104797655f72b0a455b9999d5c9a
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-05-31 10:09:45 +0530

    modified data.

--- src/Data/experienceData.js
+++ src/Data/experienceData.js
@@ -6,23 +6,6 @@ const data = [
     description:
       "Working on Khoros community. In this, we are using some technologies.",
     technologies: ["JavaScript", "React", "Freemarker", "Jquery", "HTML", "CSS"]
-  },
-  {
-    logo: "",
-    companyName: "",
-    position: "",
-    description: "",
-    technologies: [
-      "JavaScript",
-      "React",
-      "Freemarker",
-      "Jquery",
-      "HTML",
-      "CSS",
-      "Jquery",
-      "HTML",
-      "CSS"
-    ]
   }
 ];
 export default data;
