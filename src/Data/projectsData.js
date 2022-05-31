b2074f1dafa5279388ed70ba6f9540d3502091b2
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-05-31 10:41:12 +0530

    added projects data.

--- src/Data/projectsData.js
+++ src/Data/projectsData.js
@@ -1,67 +1,74 @@
 const projectsData = [
   {
     no: 1,
-    projectName: "",
-    description: "",
-    technologiesUsed: "",
-    liveAt: "",
-    githubRepo: ""
+    projectName: "React Shopping Cart",
+    description:
+      "This is a Food Cart App. In this we can add food items into the cart. And in this, food items are separated according to their type like vegetables, fruits and nuts. This app is designed with React.",
+    technologiesUsed: ["ReactJs", "HTML", "CSS", "Api", "Bootstrap"],
+    liveAt: "https://03s3u.csb.app/",
+    githubRepo: "https://github.com/manasabingi115/React-Shopping-Cart"
   },
   {
     no: 2,
-    projectName: "",
-    description: "",
-    technologiesUsed: "",
-    liveAt: "",
-    githubRepo: ""
+    projectName: "WareHouses (Redux) Project",
+    description:
+      "This is a project using Redux. We can search WareHouses with the name of Warehouse. Added filter functionality for different categories (cities, clusters and space availability). We can edit WareHouse details by clicking on the warehouse name.",
+    technologiesUsed: ["ReactJs", "Redux", "HTML", "CSS", "Api", "Bootstrap"],
+    liveAt: "https://06gfhm.csb.app/",
+    githubRepo: "https://github.com/manasabingi115/WoreHouses-Redux-project"
   },
   {
     no: 3,
-    projectName: "",
-    description: "",
-    technologiesUsed: "",
-    liveAt: "",
-    githubRepo: ""
+    projectName: "Facebook Post Model",
+    description:
+      "In this App, we can post text and GIFs. I have used Giphy API for fetching the Gifs, and also made my App responsive. Added color functionality for background. I used localStorage for persisting the data even after refreshing the page.",
+    technologiesUsed: ["ReactJs", "HTML", "CSS", "Giphy Api", "Bootstrap"],
+    liveAt: "https://react-w7phqf.stackblitz.io",
+    githubRepo: "https://github.com/manasabingi115/Facebook-Post-model-"
   },
   {
     no: 4,
-    projectName: "",
-    description: "",
-    technologiesUsed: "",
-    liveAt: "",
-    githubRepo: ""
+    projectName: "Food Recipe App",
+    description:
+      "This is a Food Recipe App. In this we can search for different types of food recipes which we want. This app is designed with React.",
+    technologiesUsed: ["ReactJs", "HTML", "CSS", "Api", "Bootstrap"],
+    liveAt: "https://yvltb.csb.app/",
+    githubRepo: "https://github.com/manasabingi115/Food-Recipe-App"
   },
   {
     no: 5,
-    projectName: "",
-    description: "",
-    technologiesUsed: "",
-    liveAt: "",
-    githubRepo: ""
+    projectName: "Todo application based on REST API",
+    description:
+      "This Todo list app is generally used to maintain our day-to-day tasks. We can add/delete/update/delete tasks. It used a third party REST API as a backend service. It is built with HTML,CSS and JavaScript. It uses REST API to fetch the data.",
+    technologiesUsed: ["JavaScript", "HTML", "CSS", "Api"],
+    liveAt: "https://web-platform-fhwhid.stackblitz.io",
+    githubRepo: "https://github.com/manasabingi115/Todo-REST-application"
   },
   {
     no: 6,
-    projectName: "",
-    description: "",
-    technologiesUsed: "",
-    liveAt: "",
-    githubRepo: ""
+    projectName: "Currency Converter",
+    description:
+      "This is a Currency-converter app used to change the currency from one country to another.",
+    technologiesUsed: ["JavaScript", "HTML", "CSS"],
+    liveAt: "https://lt1b3.csb.app/",
+    githubRepo: "https://github.com/manasabingi115/currency-converter"
   },
   {
     no: 7,
-    projectName: "",
-    description: "",
-    technologiesUsed: "",
-    liveAt: "",
-    githubRepo: ""
+    projectName: "Dictionary App",
+    description:
+      "This is a dictionary-app.In this we can find meanings of the word which we want. And also we can get pronunciation, one example sentence and origin of the word.",
+    technologiesUsed: ["JavaScript", "HTML", "CSS"],
+    liveAt: "https://5ezg5.csb.app/",
+    githubRepo: "https://github.com/manasabingi115/dictionary-app"
   },
   {
     no: 8,
-    projectName: "",
-    description: "",
-    technologiesUsed: "",
-    liveAt: "",
-    githubRepo: ""
+    projectName: "Analog Clock",
+    description: "This is a clock designed with HTML and CSS.",
+    technologiesUsed: ["HTML", "CSS"],
+    liveAt: "https://1gs6i.csb.app/",
+    githubRepo: "https://github.com/manasabingi115/Clock"
   }
 ];
 
