1645c550c37e57f1d37305b21c568ed2fa07d909
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-05-31 16:12:15 +0530

    made project cards responsive.

--- src/projects.js
+++ src/projects.js
@@ -3,34 +3,28 @@ import data from "./Data/projectsData";
 
 export default function Projects() {
   return (
-    <div id="projects">
+    <div>
       <h1>Projects</h1>
-      {data.map((data, index) => (
-        <div className="card" key={index}>
-          <div className="card-content">
-            <div className="media">
-              <div className="media-left">
-                <img
-                  className="company-logo"
-                  src=""
-                  alt="Placeholder img"
-                  width="100"
-                />
+      <div id="projects" className="columns is-multiline">
+        {data.map((data, index) => (
+          <div className="card project-card column is-one-quarter" key={index}>
+            <div className="card-content">
+              <div className="media">
+                <div className="media-content">
+                  <p className="title is-4">Project Name</p>
+                </div>
               </div>
-              <div className="media-content">
-                <p className="title is-4">Project Name</p>
+              <div className="content">
+                <p>{data.description}</p>
+                {data.technologiesUsed.map((el, index) => (
+                  <p key={index}>{el}</p>
+                ))}
+                <div className="techs-in-companies columns is-multiline is-mobile"></div>
               </div>
-            </div>
-            <div className="content">
-              <p>{data.description}</p>
-              {data.technologiesUsed.map((el, index) => (
-                <p key={index}>{el}</p>
-              ))}
-              <div className="techs-in-companies columns is-multiline is-mobile"></div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
