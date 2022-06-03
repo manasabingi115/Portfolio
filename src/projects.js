b7660b1b0ec140f3ccfee1195c7bab1a7702aeec
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-06-03 14:32:46 +0530

    added content in projects.js file.

--- src/projects.js
+++ src/projects.js
@@ -4,14 +4,14 @@ import data from "./Data/projectsData";
 export default function Projects() {
   return (
     <div>
-      <h1>Projects</h1>
+      <h1 className="projects-title">Projects</h1>
       <div id="projects" className="columns is-multiline">
         {data.map((data, index) => (
           <div className="card project-card column is-one-quarter" key={index}>
             <div className="card-content">
               <div className="media">
                 <div className="media-content">
-                  <p className="title is-4">Project Name</p>
+                  <p className="title is-4">{data.projectName}</p>
                 </div>
               </div>
               <div className="content">
@@ -19,6 +19,7 @@ export default function Projects() {
                 {data.technologiesUsed.map((el, index) => (
                   <p key={index}>{el}</p>
                 ))}
+                {/* <a href="">Link</a> */}
                 <div className="techs-in-companies columns is-multiline is-mobile"></div>
               </div>
             </div>
