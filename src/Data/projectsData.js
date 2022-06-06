ee9a405bcc7c1b1927119829646c32458ba8fb9b
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-06-06 18:31:08 +0530

    added project images.

--- src/Data/projectsData.js
+++ src/Data/projectsData.js
@@ -1,6 +1,16 @@
+import fbimg from "../Images/facebook post model.png";
+import warehouseimg from "../Images/Warehouses.png";
+import recipesimg from "../Images/Food-recipe-app.png";
+import shoppingimg from "../Images/React-shopping-cart.png";
+import currencyimg from "../Images/Currency-converter.png";
+import todoimg from "../Images/Todo-app.png";
+import clockimg from "../Images/Clock.png";
+import dictionaryimg from "../Images/dictionary-app.png";
+
 const projectsData = [
   {
     no: 1,
+    image: shoppingimg,
     projectName: "React Shopping Cart",
     description:
       "This is a Food Cart App. In this we can add food items into the cart. And in this, food items are separated according to their type like vegetables, fruits and nuts. This app is designed with React.",
@@ -10,6 +20,7 @@ const projectsData = [
   },
   {
     no: 2,
+    image: warehouseimg,
     projectName: "WareHouses (Redux) Project",
     description:
       "This is a project using Redux. We can search WareHouses with the name of Warehouse. Added filter functionality for different categories (cities, clusters and space availability). We can edit WareHouse details by clicking on the warehouse name.",
@@ -19,6 +30,7 @@ const projectsData = [
   },
   {
     no: 3,
+    image: fbimg,
     projectName: "Facebook Post Model",
     description:
       "In this App, we can post text and GIFs. I have used Giphy API for fetching the Gifs, and also made my App responsive. Added color functionality for background. I used localStorage for persisting the data even after refreshing the page.",
@@ -28,6 +40,7 @@ const projectsData = [
   },
   {
     no: 4,
+    image: recipesimg,
     projectName: "Food Recipe App",
     description:
       "This is a Food Recipe App. In this we can search for different types of food recipes which we want. This app is designed with React.",
@@ -37,6 +50,7 @@ const projectsData = [
   },
   {
     no: 5,
+    image: todoimg,
     projectName: "Todo application based on REST API",
     description:
       "This Todo list app is generally used to maintain our day-to-day tasks. We can add/delete/update tasks. It used a third party REST API as a backend service. It is built with HTML,CSS and JavaScript. It uses REST API to fetch the data.",
@@ -46,6 +60,7 @@ const projectsData = [
   },
   {
     no: 6,
+    image: currencyimg,
     projectName: "Currency Converter",
     description:
       "This is a Currency-converter app used to change the currency from one country to another.",
@@ -55,6 +70,7 @@ const projectsData = [
   },
   {
     no: 7,
+    image: dictionaryimg,
     projectName: "Dictionary App",
     description:
       "This is a dictionary-app.In this we can find meanings of the word which we want. And also we can get pronunciation, one example sentence and origin of the word.",
@@ -64,6 +80,7 @@ const projectsData = [
   },
   {
     no: 8,
+    image: clockimg,
     projectName: "Analog Clock",
     description: "This is a clock designed with HTML and CSS.",
     technologiesUsed: ["HTML", "CSS"],
--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import data from "./Data/projectsData";
+// import fbimage from "./Images/facebook post model.png";
 
 export default function Projects() {
   return (
@@ -8,11 +9,17 @@ export default function Projects() {
       <div id="projects" className="columns is-multiline">
         {data.map((data, index) => (
           <div className="card project-card column is-one-quarter" key={index}>
+            <div className="card-image">
+              <figure className="image is-4by3">
+                <img src={data.image} alt="Placeholder img" />
+              </figure>
+            </div>
             <div className="card-content">
               <div className="media">
                 <div className="media-content">
                   <p className="title is-4">{data.projectName}</p>
                 </div>
+                {/* <img src={data.image} /> */}
               </div>
               <div className="content">
                 <p>{data.description}</p>
